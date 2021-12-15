b3721f0e33253f59bf7c2b0bc063d66d00cbc1fc
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2021-12-16 01:54:53 +0200

    Added site struct

--- server/models/site.go
+++ server/models/site.go
@@ -3,7 +3,6 @@ package models
 type Site struct {
 	ID       int64  `json:"id"`
 	Title    string `json:"name"`
-	Email    string `json:"email"`
 	Url      string `json:"url"`
 	Interval string `json:"interval"`
 }
