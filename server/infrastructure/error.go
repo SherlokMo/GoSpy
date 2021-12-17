aa52c394dfeb3b8e47f467a344311c0bfc0b360c
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2021-12-17 02:20:47 +0200

    added infrastructure methods

--- server/infrastructure/error.go
+++ server/infrastructure/error.go
@@ -1,7 +1,15 @@
 package infrastructure
 
+import "net/http"
+
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func ControllerErrorResponder(err error, w http.ResponseWriter, status int) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
 	}
 }
--- server/infrastructure/json.go
+++ server/infrastructure/json.go
@@ -3,6 +3,7 @@ package infrastructure
 import (
 	"encoding/json"
 	"fmt"
+	"gospy/models"
 	"net/http"
 )
 
@@ -11,4 +12,12 @@ func JsonResponse(res interface{}, w http.ResponseWriter) {
 	CheckError(err)
 
 	fmt.Fprint(w, string(response))
+}
+
+func ParseBody(model models.Model, w http.ResponseWriter, request *http.Request) models.Model {
+	err := json.NewDecoder(request.Body).Decode(&model)
+
+	ControllerErrorResponder(err, w, http.StatusBadGateway)
+
+	return model
 }
