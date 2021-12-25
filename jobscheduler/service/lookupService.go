18317d730b2b3f4115e062569e742b5682b0a7c2
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2021-12-26 00:31:39 +0200

    Added lookup service

---
+++ jobscheduler/service/lookupService.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"fmt"
+	"jobscheduler/helpers"
+	"jobscheduler/infrastructure"
+	"jobscheduler/models"
+)
+
+type LookUpService struct {
+	lookup *models.Lookup
+}
+
+func NewLookUpService() *LookUpService {
+	return &LookUpService{}
+}
+
+func (p *LookUpService) EmbedModel(s *models.Lookup) *LookUpService {
+	p.lookup = s
+	fmt.Println(p.lookup)
+	return p
+}
+
+func (p LookUpService) Save() {
+	id := infrastructure.Pgsql.Insert("INSERT INTO lookup (site_id, dnslookup, connection, tlshandshake, warning, status_code) VALUES($1, $2, $3, $4, $5, $6)", p.lookup.Site_id, p.lookup.DNSLookUp, p.lookup.ConnectionTime, p.lookup.TLSHandshake, helpers.NewNullString(p.lookup.Warning), p.lookup.Status)
+	p.lookup.ID = id
+}
