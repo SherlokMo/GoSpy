4e392941c12c959cc2fb109a936bff76fc06b9c1
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2021-12-26 01:26:40 +0200

    Handled client queue instance

--- jobscheduler/main.go
+++ jobscheduler/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"jobscheduler/infrastructure"
+	"jobscheduler/queueClient"
 	"jobscheduler/scheduler"
 )
 
 func main() {
 	infrastructure.HandlePostgre()
+	queueClient.HandleClient()
 	scheduler.RunScheduler()
 }
