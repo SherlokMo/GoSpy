6eceb31391639c513e4f22e237e1e9c68d9a8769
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2022-01-06 03:52:14 +0200

    Dispatched checks router handler in the entry point

--- app/main.go
+++ app/main.go
@@ -15,6 +15,8 @@ func handleRequests() {
 	router.Use(mux.CORSMethodMiddleware(router))
 
 	handler.HandleSitesRequest(router)
+	handler.HandleLookupRequest(router)
+
 	log.Println("Listening at ::8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
